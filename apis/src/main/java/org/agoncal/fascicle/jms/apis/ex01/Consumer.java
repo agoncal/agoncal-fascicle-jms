ec10468f791298d35afd15ae700b5320e0fadd89
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-09-14 14:26:37 +0200

    apis

---
+++ apis/src/main/java/org/agoncal/fascicle/jms/apis/ex01/Consumer.java
@@ -0,0 +1,44 @@
+package org.agoncal.fascicle.jms.apis.ex01;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+// tag::adocsnippet[]
+public class Consumer {
+
+  public static void main(String[] args) {
+
+    try {
+      // Gets the JNDI context
+      Context jndiContext = new InitialContext();
+
+      // Looks up the administered objects
+      ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("jms/fascicle/ConnectionFactory");
+      Destination queue = (Destination) jndiContext.lookup("jms/fascicle/Queue");
+
+      // Creates the needed artifacts to connect to the queue
+      Connection connection = connectionFactory.createConnection();
+      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      MessageConsumer consumer = session.createConsumer(queue);
+      connection.start();
+
+      // Loops to receive the messages
+      System.out.println("\nInfinite loop. Waiting for a message...");
+      while (true) {
+        TextMessage message = (TextMessage) consumer.receive();
+        System.out.println("Message received: " + message.getText());
+      }
+
+    } catch (NamingException | JMSException e) {
+      e.printStackTrace();
+    }
+  }
+}
+// end::adocsnippet[]
