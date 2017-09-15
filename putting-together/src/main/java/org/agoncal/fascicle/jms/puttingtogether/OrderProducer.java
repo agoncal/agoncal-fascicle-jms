a5fb74505da67c51f9bab333b9b21195963b33ad
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-09-15 09:02:39 +0200

    putting together

---
+++ putting-together/src/main/java/org/agoncal/fascicle/jms/puttingtogether/OrderProducer.java
@@ -0,0 +1,46 @@
+package org.agoncal.fascicle.jms.puttingtogether;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Date;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+// tag::adocsnippet[]
+public class OrderProducer {
+
+  public static void main(String[] args) throws NamingException {
+
+    if (args.length != 1) {
+      System.out.println("usage : enter an amount");
+      System.exit(0);
+    }
+
+    System.out.println("Sending message with amount = " + args[0]);
+
+    // Creates an orderDto with a total amount parameter
+    Float totalAmount = Float.valueOf(args[0]);
+    OrderDTO order = new OrderDTO(1234l, new Date(), "Serge Gainsbourg", totalAmount);
+
+    // Gets the JNDI context
+    Context jndiContext = new InitialContext();
+
+    // Looks up the administered objects
+    ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("jms/fascicle/ConnectionFactory");
+    Destination topic = (Destination) jndiContext.lookup("jms/fascicle/Topic");
+
+    try (JMSContext jmsContext = connectionFactory.createContext()) {
+      // Sends an object message to the topic
+      jmsContext.createProducer().setProperty("orderAmount", totalAmount).send(topic, order);
+      System.out.println("\nOrder sent : " + order.toString());
+    }
+  }
+}
+// end::adocsnippet[]
